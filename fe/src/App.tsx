a35293d1d78ca1190996c7cdd919e78818d1121b
omittee <2503868640@qq.com>
2022-10-04 22:04:35 +0800

    scroll and simplify

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import AboutBoard from "./components/About/AboutBoard"
-import ArticleBoard from './components/Article/ArticleBoard';
+import React from "react";
+import AboutBoard from "./components/About/AboutBoard";
+import ArticleBoard from "./components/Article/ArticleBoard";
+import "@/assets/CSS/app.scss"
 
 function App() {
   return (
-    <>
+    <div className="app">
       <AboutBoard></AboutBoard>
       <ArticleBoard></ArticleBoard>
-    </>
+    </div>
   );
 }
 
