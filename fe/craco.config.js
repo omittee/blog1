44a538fe6d607c91f03ee1f7f8e24c2cc1302fab
omittee <2503868640@qq.com>
2022-10-09 11:52:11 +0800

    update decorativeArticle

--- fe/craco.config.js
+++ fe/craco.config.js
@@ -1,8 +1,10 @@
 const path = require('path');
 module.exports = {
   webpack: {
+    extensions: ['.ts', '.json', '.scss'],
     alias: {
       '@': path.resolve(__dirname, 'src'),
+      '@public': path.resolve(__dirname, 'public')
     },
   },
 };
